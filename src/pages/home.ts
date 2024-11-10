2ed4c2edf9873becd86f969f076b979c977ccd24
Fillipe143 <fillipedev.contato@gmail.com>
2024-11-10 10:14:47 -0300

    adicionado exibição de dados do usuario e logout

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -6,5 +6,9 @@ import * as auth from "../database/auth";
 
 auth.onUserChange(user => {
     if (!user) return;
+    if (user.displayName) $("#username").text(user.displayName);
+    if (user.photoURL) $("#userphoto").attr("src", user.photoURL);
+
     $(document.body).removeClass("dismiss");
+    $("#exit").on("click", _ => auth.logout());
 })
