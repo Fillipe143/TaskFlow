07a02b5224acb2409b4a42daf7ace158b7127ecb
Fillipe143 <fillipedev.contato@gmail.com>
2024-11-10 08:38:13 -0300

    Adicionado componente sidebar

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,9 +1,8 @@
 import $ from "jquery";
+import "../components/sidebar";
 import * as auth from "../database/auth";
 
 auth.onUserChange(user => {
     if (!user) return;
     $(document.body).removeClass("dismiss");
-    $("h1").text(`Seja bem vindo ${user.displayName}!`);
-    $("input").on("click", () => auth.logout());
 })
