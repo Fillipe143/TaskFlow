38949f5e14162a3787afb35e826b593d29fe539f
Fillipe143 <fillipedev.contato@gmail.com>
2024-11-17 10:21:07 -0300

    adicionado escopo proprio para as variaveis

--- src/components/resizable/script.ts
+++ src/components/resizable/script.ts
@@ -1,67 +1,69 @@
-const containers = document.getElementsByClassName("resizable-container");
+{
+    const containers = document.getElementsByClassName("resizable-container");
 
-const minSizeProportion = .2; // 20%
-let isResizing = false;
+    const minSizeProportion = .2; // 20%
+    let isResizing = false;
 
-for (const container of containers) {
-    const childs = container.querySelectorAll(":scope > .resize-child") as NodeListOf<HTMLElement>;
-    const handlers = container.querySelectorAll(":scope > .resize-handle") as NodeListOf<HTMLElement>;
+    for (const container of containers) {
+        const childs = container.querySelectorAll(":scope > .resize-child") as NodeListOf<HTMLElement>;
+        const handlers = container.querySelectorAll(":scope > .resize-handle") as NodeListOf<HTMLElement>;
 
-    for (let i = 0; i < handlers.length; i++) {
-        resize(container as HTMLElement, handlers[i], childs[i], childs[i + 1], childs.length);
+        for (let i = 0; i < handlers.length; i++) {
+            resize(container as HTMLElement, handlers[i], childs[i], childs[i + 1], childs.length);
+        }
     }
-}
 
-function resize(container: HTMLElement, handler: HTMLElement, firstChild: HTMLElement, secondChild: HTMLElement, childrensCount: number) {
-    handler.addEventListener("mousedown", e => {
-        e.preventDefault();
-        isResizing = true;
+    function resize(container: HTMLElement, handler: HTMLElement, firstChild: HTMLElement, secondChild: HTMLElement, childrensCount: number) {
+        handler.addEventListener("mousedown", e => {
+            e.preventDefault();
+            isResizing = true;
 
-        const isVertical = container.classList.contains("vertical");
-        const minSize = (isVertical ? container.offsetHeight : container.offsetWidth) * minSizeProportion;
+            const isVertical = container.classList.contains("vertical");
+            const minSize = (isVertical ? container.offsetHeight : container.offsetWidth) * minSizeProportion;
 
-        const startPosition = isVertical ? e.clientY : e.clientX;
-        const firstChildStartSize = isVertical ? firstChild.offsetHeight : firstChild.offsetWidth;
-        const secondChildStartSize = isVertical ? secondChild.offsetHeight : secondChild.offsetWidth;
+            const startPosition = isVertical ? e.clientY : e.clientX;
+            const firstChildStartSize = isVertical ? firstChild.offsetHeight : firstChild.offsetWidth;
+            const secondChildStartSize = isVertical ? secondChild.offsetHeight : secondChild.offsetWidth;
 
-        const mousemoveEvent = (e: MouseEvent) => {
-            if (!isResizing) return;
+            const mousemoveEvent = (e: MouseEvent) => {
+                if (!isResizing) return;
 
-            const dp = startPosition - (isVertical ? e.clientY : e.clientX);
-            let firstChildSize = firstChildStartSize - dp;
-            let secondChildSize = secondChildStartSize + dp;
+                const dp = startPosition - (isVertical ? e.clientY : e.clientX);
+                let firstChildSize = firstChildStartSize - dp;
+                let secondChildSize = secondChildStartSize + dp;
 
-            if (firstChildSize < minSize) {
-                secondChildSize -= minSize - firstChildSize;
-                firstChildSize = minSize;
-            }
-            if (secondChildSize < minSize) {
-                firstChildSize -= minSize - secondChildSize;
-                secondChildSize = minSize;
-            }
+                if (firstChildSize < minSize) {
+                    secondChildSize -= minSize - firstChildSize;
+                    firstChildSize = minSize;
+                }
+                if (secondChildSize < minSize) {
+                    firstChildSize -= minSize - secondChildSize;
+                    secondChildSize = minSize;
+                }
 
-            firstChildSize -= 1;
-            secondChildSize -= 1;
+                firstChildSize -= 1;
+                secondChildSize -= 1;
 
-            firstChild.style.flex = "none";
-            secondChild.style.flex = "none";
+                firstChild.style.flex = "none";
+                secondChild.style.flex = "none";
 
-            if (isVertical) {
-                firstChild.style.height = `${firstChildSize}px`;
-                secondChild.style.height = `${secondChildSize}px`;
-            } else {
-                firstChild.style.width = `${firstChildSize}px`;
-                secondChild.style.width = `${secondChildSize}px`;
-            }
-        };
+                if (isVertical) {
+                    firstChild.style.height = `${firstChildSize}px`;
+                    secondChild.style.height = `${secondChildSize}px`;
+                } else {
+                    firstChild.style.width = `${firstChildSize}px`;
+                    secondChild.style.width = `${secondChildSize}px`;
+                }
+            };
 
-        const mouseupEvent = () => {
-            isResizing = false;
-            document.removeEventListener("mousemove", mousemoveEvent);
-            document.removeEventListener("mouseup", mouseupEvent);
-        };
+            const mouseupEvent = () => {
+                isResizing = false;
+                document.removeEventListener("mousemove", mousemoveEvent);
+                document.removeEventListener("mouseup", mouseupEvent);
+            };
 
-        document.addEventListener("mousemove", mousemoveEvent);
-        document.addEventListener("mouseup", mouseupEvent);
-    });
+            document.addEventListener("mousemove", mousemoveEvent);
+            document.addEventListener("mouseup", mouseupEvent);
+        });
+    }
 }
