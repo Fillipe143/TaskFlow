f15ce89c4edc416370c47dfee6b9cb57d30ac1c3
Kalil-S <smithkalil04@gmail.com>
2024-11-21 20:40:23 -0300

    Add files via upload

---
+++ sidebar.ts
@@ -0,0 +1,52 @@
+import $ from "jquery";
+import { SidebarResizeObservable } from './src/components/SidebarResizeObservable';
+
+let isResizing = false;
+const minWidth = 200;
+const sidebar = $(".sidebar");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebarElement = document.getElementById('sidebar');
+
+    if (!sidebarElement) {
+        console.error('Elemento da sidebar não encontrado.');
+        return;
+    }
+
+    const sidebarObserver = new SidebarResizeObservable(sidebarElement);
+
+    // Adicione observadores para tratar as mudanças
+    sidebarObserver.addObserver((dimensions: DOMRectReadOnly) => {
+        console.log('Sidebar alterada:', dimensions);
+        handleSidebarResize(dimensions);
+    });
+
+    function handleSidebarResize(dimensions: DOMRectReadOnly): void {
+        const { width, height } = dimensions;
+        console.log(`Largura: ${width}px, Altura: ${height}px`);
+        // Aqui você pode adicionar lógica adicional, como atualizar o número de colunas ou layout
+    }
+});
+
+$(".resize-handle").on("mousedown", e => {
+    e.preventDefault();
+    isResizing = true;
+
+    const startX = e.clientX;
+    const startWidth = sidebar.outerWidth() || 0;
+
+    $(document).on("mousemove", e => {
+        if (isResizing) {
+            const dx = e.clientX - startX;
+            const maxWidth = ($(window).outerWidth() || 0)/ 2;
+            const width = Math.max(Math.min(startWidth + dx, maxWidth), minWidth);
+            sidebar.css("width", width);
+        }
+    });
+
+    $(document).on("mouseup", _ => {
+        isResizing = false;
+        $(document).off("mousemove");
+        $(document).off("mouseup");
+    });
+});
